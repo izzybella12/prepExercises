8f001a042253d8d1e525f01f9d98dbe2a7bf28b8
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-07-03 11:13:43 -0400

    understanding reverse method

---
+++ splitReverseMethods.js
@@ -0,0 +1,7 @@
+let arr1 = "john".split("");
+let arr2 = arr1.reverse();
+let arr3 = "jones".split("");
+arr2.push(arr3);
+console.log("Array 1", arr1)
+console.log("Array 2", arr2)
+console.log("Array 3", arr3)
