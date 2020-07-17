3f69ed5332541ee9d560477e1ffa7243a0462dd2
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-07-17 19:45:31 -0400

    Calendar attempt

---
+++ talkingCalendar10.js
@@ -0,0 +1,28 @@
+// YYYY/MM/DD
+
+const talkingCalendar = function(date) {
+
+  Months = {
+    1: "January", 
+    2: "Febryary", 
+    3: "March", 
+    4: "April", 
+    5: "May", 
+    6: "June", 
+    7: "July", 
+    8: "August", 
+    9: "September", 
+    10: "October", 
+    11: "November", 
+    12: "December"
+  };
+
+  month = 0; 
+  let splitDate = date.split("");
+
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
+
