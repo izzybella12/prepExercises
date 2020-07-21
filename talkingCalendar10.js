8f641450acdb4b72b248392657150504375f168d
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-07-21 19:49:17 -0400

    Trying different method for calendar

--- talkingCalendar10.js
+++ talkingCalendar10.js
@@ -3,26 +3,66 @@
 const talkingCalendar = function(date) {
 
   let months = {
-    01: "January", 
-    02: "Febryary", 
-    03: "March", 
-    04: "April", 
-    05: "May", 
-    06: "June", 
-    07: "July", 
-    08: "August", 
-    09: "September", 
-    10: "October", 
-    11: "November", 
-    12: "December"
-  };
+    January: 01,  
+    February: 02, 
+    March: 03, 
+    April: 04, 
+    May: 05, 
+    June: 06, 
+    July: 07, 
+    August: 08, 
+    September: 09, 
+    October: 10, 
+    November: 11, 
+    December: 12
+  }
 
-  let writtenDate = "";
-  let month; 
-  let splitDate = date.split("");
-  month = splitDate[5] += splitDate[6]
-  writtenDate += months[Number(month)]
-  console.log(writtenDate);
+  let numbers = { 
+    '1st': '01',
+    '2nd': '02',
+    '3rd': '03',
+    '4th': '04',
+    '5th': '05',
+    '6th': '06',
+    '7th': '07',
+    '8th': '08',
+    '9th': '09',
+    '10th': '10',
+    '11th': '11',
+    '12th': '12',
+    '13th': '13',
+    '14th': '14',
+    '15th': '15',
+    '16th': '16',
+    '17th': '17',
+    '18th': '18',
+    '19th': '19',
+    '20th': '20',
+    '21st': '21',
+    '22nd': '22',
+    '23rd': '23',
+    '24th': '24',
+    '25th': '25',
+    '26th': '26',
+    '27th': '27',
+    '28th': '28',
+    '29th': '29',
+    '30th': '30',
+    '31st': '31'
+  }
+
+  // let writtenDate = "";
+  // let month; 
+  // let day; 
+  // let splitDate = date.split("/");
+  // console.log(splitDate);
+  // month = splitDate[5] 
+  // month += splitDate[6]
+  // writtenDate += months[Number(month)] += " "
+  // writtenDate += splitDate[8]
+  // writtenDate += splitDate[9]
+  // console.log(writtenDate);
+  // day = splitDate[]
 
 };
 
